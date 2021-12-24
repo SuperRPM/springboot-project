bad11fc46e48fd01f8ae10722e868dbd784a8336
SuperRPM <vidafirmar@gmail.com>
2021-12-25 00:19:09 +0900

    unit test

---
+++ src/main/java/com/rpm/admin/Application.java
@@ -0,0 +1,12 @@
+package com.rpm.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
