26a828e3d5f39e1713f26046311254c4dec3823c
SuperRPM <vidafirmar@gmail.com>
2021-12-29 13:34:28 +0900

    게시글 수정 기능 추가

--- src/main/java/com/rpm/admin/web/IndexController.java
+++ src/main/java/com/rpm/admin/web/IndexController.java
@@ -2,11 +2,13 @@ package com.rpm.admin.web;
 
 import com.rpm.admin.domain.posts.PostsRepository;
 import com.rpm.admin.service.posts.PostsService;
+import com.rpm.admin.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -23,5 +25,13 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post", dto);
+
+        return "posts-update";
     }
 }
