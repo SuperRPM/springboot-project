5cf9aba13fad125d366d8fe395f1c16a90d3b727
SuperRPM <vidafirmar@gmail.com>
2022-02-21 16:49:40 +0900

    google login and annotation

--- src/main/java/com/rpm/admin/web/IndexController.java
+++ src/main/java/com/rpm/admin/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.rpm.admin.web;
 
+import com.rpm.admin.config.auth.LoginUser;
 import com.rpm.admin.config.auth.dto.SessionUser;
 import com.rpm.admin.domain.posts.PostsRepository;
 import com.rpm.admin.service.posts.PostsService;
@@ -17,12 +18,12 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+//    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
