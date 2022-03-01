1ed95ebb907e36ee8eac91f1da8e1c002002e707
SuperRPM <vidafirmar@gmail.com>
2022-03-01 15:58:04 +0900

    apply security to already exist test(exeption: mainpage_loading)

--- src/main/java/com/rpm/admin/Application.java
+++ src/main/java/com/rpm/admin/Application.java
@@ -2,10 +2,8 @@ package com.rpm.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
